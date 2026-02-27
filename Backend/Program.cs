de49de0b4496b765c35f9921132fbfc75f2ba624
Gustavo Gomes <lgglima@gmail.com>
2026-02-27 17:44:07 -0300

    change sql server to use postgresql.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
